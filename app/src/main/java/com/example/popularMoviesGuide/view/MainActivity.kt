acc1359eedcca82f9d6627aec9bfc6a5f43d7ba1
@revakovsky <revakovsky@gmail.com>
2022-10-07 21:11:30 +0300

    initial commit in the clean architecture branch

--- app/src/main/java/com/example/popularMoviesGuide/view/MainActivity.kt
+++ app/src/main/java/com/example/popularMoviesGuide/view/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : AppCompatActivity() {
         val intentToAnotherScreen = Intent(this, MoviesActivity::class.java)
         startActivity(intentToAnotherScreen)
 
-        //openRegistrationScreen()
+        openRegistrationScreen()
     }
 
     private fun openRegistrationScreen() {
